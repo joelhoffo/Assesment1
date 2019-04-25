fd84bc0a2e657836025a444ad7e97134a2b36b5d
joelhoffo <47997129+joelhoffo@users.noreply.github.com>
2019-04-25 11:03:35 +1000

    Update DecryptionRotationCipher.c

--- DecryptionRotationCipher.c
+++ DecryptionRotationCipher.c
@@ -1,39 +1,22 @@
 #include<stdio.h>
  
-int main()
+void DecrptUsingRotationCypher(int key)
 {
-	char message[100], ch;
-	int i, key;
-	
-	printf("Enter a message to decrypt: ");
-	gets(message);
-	printf("Enter key: ");
-	scanf("%d", &key);
-	
-	for(i = 0; message[i] != '\0'; ++i){
-		ch = message[i];
-		
-		if(ch >= 'a' && ch <= 'z'){
-			ch = ch - key;
-			
-			if(ch < 'a'){
-				ch = ch + 'z' - 'a' + 1;
-			}
-			
-			message[i] = ch;
-		}
-		else if(ch >= 'A' && ch <= 'Z'){
-			ch = ch - key;
-			
-			if(ch < 'A'){
-				ch = ch + 'Z' - 'A' + 1;
-			}
-			
-			message[i] = ch;
-		}
-	}
-	
-	printf("Decrypted message: %s", message);
-	
-	return 0;
+    int eof = 0;
+    for (int a = 0; _encryptedText[a] != 0; ++a)
+    {
+        eof = a;
+        const char encryptedChar = _encryptedText[a];
+        if (encryptedChar==' ')
+        _unEncryptedText[a] = encryptedChar;
+        else
+        
+        //if (encryptedChar >= 'a' && encryptedChar <= 'z' 
+        //    || encryptedChar >= 'A' && encryptedChar <= 'Z')
+        _unEncryptedText[a] = encryptedChar - key;
+    }
+    for (int b = eof + 1; b < 1000; b++)
+        _unEncryptedText[b] = 0;
+
+    printf("\nDecrypted message: %s", _unEncryptedText);
 }
