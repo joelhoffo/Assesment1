8dc7e3bc36beac6c4ea01e01b986416ab40cab82
joelhoffo <47997129+joelhoffo@users.noreply.github.com>
2019-04-28 08:20:25 +1000

    Update NokeysubRot.c

--- NokeysubRot.c
+++ NokeysubRot.c
@@ -10,7 +10,7 @@ void DescryptUsingRotationCipherWithNoKey()
     
     char input[1000]="TVU TVAOTH: AOL KHAH IYVBNOA AV BZ IF AOL IVAOHU ZWPLZ WPUWVPUAZ AOL LEHJA SVJHAPVU VM AOL LTWLYVY'Z ULD IHAASL ZAHAPVU. DL HSZV RUVD AOHA AOL DLHWVU ZFZALTZ VM AOPZ KLHAO ZAHY HYL UVA FLA VWLYHAPVUHS. DPAO AOL PTWLYPHS MSLLA ZWYLHK AOYVBNOVBA AOL NHSHEF PU H CHPU LMMVYA AV LUNHNL BZ, PA PZ YLSHAPCLSF BUWYVALJALK. IBA TVZA PTWVYAHUA VM HSS, DL'CL SLHYULK AOHA AOL LTWLYVY OPTZLSM PZ WLYZVUHSSF VCLYZLLPUN AOL MPUHS ZAHNLZ VM AOL JVUZAYBJAPVU VM AOPZ KLHAO ZAHY. THUF IVAOHUZ KPLK AV IYPUN BZ AOPZ PUMVYTHAPVU.";
     char AmountC;
-    int A, key=7;
+    int A, ;
 	
 
 	
@@ -18,12 +18,11 @@ void DescryptUsingRotationCipherWithNoKey()
 		AmountC = input[A];
 		
 		if( AmountC >= 'a' && AmountC <= 'z'){
-			AmountC = AmountC - key;
+			AmountC = AmountC - 7;
 			
-			input[A] = AmountC;
 		}
 		else if(AmountC >= 'A' && AmountC <= 'Z'){
-			AmountC = AmountC - key;
+			AmountC = AmountC - 7;
 			
 			if(AmountC < 'A'){
 				AmountC = AmountC + 'Z' - 'A' + 1;
